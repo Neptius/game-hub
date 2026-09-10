c13a995f71deeb46c9c3f4d47400853941423f06
Aymeric <aymeric.legros@adixon.fr>
2026-09-10 15:43:46 +0200

    up

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :game_hub, GameHub.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "pass",
   hostname: "localhost",
   database: "game_hub_dev",
   stacktrace: true,
